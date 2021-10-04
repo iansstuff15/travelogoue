2ce88c7a2a8835b852c780405456f67c9e8b2acf
amaiYume <72297102+amaiYume@users.noreply.github.com>
2021-10-04 15:21:34 +0800

    Add files via upload

--- src/components/booking/TextFieldCus.jsx
+++ src/components/booking/TextFieldCus.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 import './TextFieldCus.scss';
 import TextField from '@mui/material/TextField';
 
-const TextFieldCus = ({name}) => (
+const TextFieldCus = ({name, style}) => (
     <>
    
 
-            <TextField className = "tField" id="filled-basic" label={name} variant="filled" required color="warning"/>
+            <TextField style = {style}className = "tField" id="filled-basic" label={name} variant="filled" required color="warning"/>
 
 
 
