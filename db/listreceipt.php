20dc7fda62fa599c693054b4f8b5c0582a1c72d1
KrisD00 <61658901+KrisD00@users.noreply.github.com>
2021-10-04 17:46:41 +0800

    Update listreceipt.php

--- db/listreceipt.php
+++ db/listreceipt.php
@@ -1,9 +1,11 @@
 <?php
-/*To Get All Items in the Table (list.php).*/
 header('Content-Type: application/json');
-
+header("Access-Control-Allow-Origin:*");
 include("db.php");
-
+/*$id = $_POST['id'];
+// Only display logged in user's receipt
+$stmt = $db->prepare("SELECT * FROM receipt WHERE id=?");
+$stmt->execute($id);*/
 $stmt = $db->prepare("SELECT * FROM receipt");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
