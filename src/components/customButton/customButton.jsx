dab54a92e247f0d763bdd0d85c527f4ace7329aa
amaiYume <72297102+amaiYume@users.noreply.github.com>
2021-10-05 14:11:44 +0800

    updated custom button

--- src/components/customButton/customButton.jsx
+++ src/components/customButton/customButton.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 import './customButton.css'
 
-const CustomButton = ({Text}) => (
-    <>
-    <input type="submit" className="Button" value={Text}/>
-
-    </>
-)
+const CustomButton = ({Text, Type, gonextStep}) => {
+    if(Type === "submit"){
+        return(
+            <>
+                <input type={Type} className="Button" value={Text}/>
+            </>
+        )
+    }else{
+        return(
+            <>
+                <input type={Type} className="Button" value={Text} onClick = {(e) => gonextStep(e)}/>
+            </>
+        )
+    }
+}
 
 export default  CustomButton
