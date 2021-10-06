13ba469d0ff027076ec4637ee07f0d85d3f93bba
KrisD00 <61658901+KrisD00@users.noreply.github.com>
2021-10-06 23:48:16 +0800

    Update signup.jsx
    Working

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -2,17 +2,17 @@ import React , {Component} from 'react';
 import Title from '../../components/title/title';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-
+import axios from 'axios'
 import send from '../../assets/send.svg'
 import './signup.css'
 import CustomButton from '../../components/customButton/customButton';
 import { borderRadius } from '@mui/system';
 class signup extends Component {
  
-    constructor(props){
+  constructor(props){
       super(props);
-      this.onChangefirstname= this.onChangefirstname.bind(this);
-      this.onChangelastname= this.onChangelastname.bind(this);
+      this.onChangefirst_name= this.onChangefirst_name.bind(this);
+      this.onChangelast_name= this.onChangelast_name.bind(this);
       this.onChangeEmail= this.onChangeEmail.bind(this);
       this.onChangePassword= this.onChangePassword.bind(this);
       this.submitForm = this.submitForm.bind(this);
@@ -27,31 +27,26 @@ class signup extends Component {
       }
     }
      
-    onChangefirstname(e){
+    onChangefirst_name(e){
       this.setState({
-        name:e.target.value
+        first_name:e.target.value
       });
     }
-    onChangelastname(e){
+    onChangelast_name(e){
         this.setState({
-          name:e.target.value
+          last_name:e.target.value
         });
       }
-     
-    onChangeEmail(e){
+     onChangeEmail(e){
       this.setState({
         email:e.target.value
       });
     }
-     
     onChangePassword(e){
       this.setState({
         password:e.target.value
       });
     }
-     
-
-     
     submitForm(e){
       e.preventDefault();
       const obj ={
@@ -60,17 +55,28 @@ class signup extends Component {
         email:this.state.email,
         password:this.state.password,
       };
-     
-      console.log(obj);
+      axios.post('http://localhost/db/createuser.php',obj)
+    .then(res=> console.log(res.data))
+    .catch(error => {
+      console.log(error.response)
+  });
+  this.setState({
+    first_name: '',
+    last_name: '',
+    email:'',
+    password:'',
+  })
+      
+
     }
-    //form wont accept input
+
     render(){
       return (
         <div className="content">
         <Title title="TELL US SOMETHING" subtitle="ABOUT YOURSELF"/>    
         <form className="form" onSubmit={this.submitForm} > 
-        <TextField id="filled-basic" label="First Name" variant="filled" required color="warning" value={this.state.first_name} onChange={this.onChangefirstname} placeholder="First name" />
-        <TextField id="filled-basic" label="Last Name" variant="filled" required  color="warning" value={this.state.last_name} onChange={this.onChangelastname} placeholder="last name" />
+        <TextField id="filled-basic" label="First Name" variant="filled" type="text" required color="warning" value={this.state.first_name} onChange={this.onChangefirst_name} placeholder="First name" />
+        <TextField id="filled-basic" label="Last Name" variant="filled" type="text" required  color="warning" value={this.state.last_name} onChange={this.onChangelast_name} placeholder="last name" />
         <TextField id="filled-basic" label="Email" variant="filled" type="email" required color="warning" value={this.state.email} onChange={this.onChangeEmail} placeholder="email"/>
         <TextField id="filled-basic" label="Password" variant="filled" type="password" required color="warning" value={this.state.password} onChange={this.onChangePassword} placeholder="password"/>
         <CustomButton Type = "submit" Text = "SIGN UP"/>
