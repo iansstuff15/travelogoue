d82eaa476514ba0dbb6c1889b0dab0a5ec9e5f11
amaiYume <72297102+amaiYume@users.noreply.github.com>
2021-10-09 23:28:35 +0800

    Add files via upload

--- src/components/image/image.jsx
+++ src/components/image/image.jsx
@@ -2,23 +2,18 @@ import React from 'react'
 
 import './image.css'
 
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 
 const Image = ({data}) => (
     <>
-    <div className="container">
-     
+    <div className = "rowPictures">
         {data.map((data)=>(
-            <div>
-              
-                {console.log(data)}
-               
-                <img src={`${data.thumbnailUrl}`} alt="place-image"/>
-            </div>
-            ))}
-  
-        
-    
+                <div key = {data.ImageId}>
+                    <img className = "imageRow" src={`${data.thumbnailUrl}`} alt={`${data.ImageId}`}/>
+                </div>
+                )
+            )
+        }
+
     </div>
     </>
 )
