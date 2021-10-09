1ae4fbb86ddfe03c5e942cae5db97fc18c1d3d9e
KrisD00 <61658901+KrisD00@users.noreply.github.com>
2021-10-10 01:23:39 +0800

    Update listreceipt.php

--- db/listreceipt.php
+++ db/listreceipt.php
@@ -1,15 +1,26 @@
 <?php
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS');
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
 include("db.php");
-/*$id = $_POST['id'];
-// Only display logged in user's receipt
-$stmt = $db->prepare("SELECT * FROM receipt WHERE id=?");
-$stmt->execute($id);*/
-$stmt = $db->prepare("SELECT * FROM receipt");
+$email=$_POST['email'];
+$stmt = $db->prepare("SELECT * FROM receipt WHERE email= '$email'");
 $stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-echo json_encode($result);
+
+if($result){
+    echo json_encode([
+        'code' => '200'
+    ]);
+    echo json_encode($result);
+} else{
+    echo json_encode([
+        'code' => '400'
+        ]);
+};
 
 ?>
