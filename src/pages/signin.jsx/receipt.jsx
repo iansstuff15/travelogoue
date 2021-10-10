a7f2ffeb4d0fd7d7303aebae607e857f129bebfe
KrisD00 <61658901+KrisD00@users.noreply.github.com>
2021-10-10 15:18:35 +0800

    Update receipt.jsx

--- src/pages/signin.jsx/receipt.jsx
+++ src/pages/signin.jsx/receipt.jsx
@@ -15,24 +15,23 @@ import { render } from '@testing-library/react';
 const Axios = axios.create({
     baseURL: 'http://localhost/db/',
 });
+function getreceipt(){
+  const arrreceipt=JSON.parse(localStorage.getItem('receipt'))
+  return arrreceipt
+}
 class SignIn extends Component {
  
     constructor(props){
         super(props);
-        this.onChangeEmail= this.onChangeEmail.bind(this);
         this.submitForm = this.submitForm.bind(this);
         this.state ={
           email:'',
-       
+          data:[],
+
         }
       }
 
 
-       onChangeEmail(e){
-        this.setState({
-          email:e.target.value
-        });
-      }
       submitForm(e){
         e.preventDefault();
         const arr=JSON.parse(localStorage.getItem('user'))
@@ -41,18 +40,15 @@ class SignIn extends Component {
           email:emailloc,
         };
         Axios.post('listreceipt.php',obj)
-      .then(res=> {console.log(res.data)
+      .then(res=> {this.state.data=res.data
+        localStorage.setItem('receipt',JSON.stringify(this.state.data))
       }
       )
       .catch(error => {
         console.log(error.response)
     });
-    this.setState({
-      email:'',
-      password:'',
-    })
         
-  
+    window.location.reload();
       }
 
     
@@ -64,15 +60,51 @@ return(
         <form className="form" onSubmit={this.submitForm}> 
         <CustomButton Type = "submit" Text = "RECEIPT"/>
         </form>
-       
+    {localStorage.getItem('receipt')
+    &&
+    <table>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>ID</th>
+              <th>Flight Number</th>
+              <th>Passenger Name</th>
+              <th>fr_location</th>
+              <th>to_location</th>
+              <th>Class</th>
+              <th>Flight Date</th>
+              <th>Flight Time</th>
+              <th>Gate Number</th>
+              <th>Board Time</th>
+              <th>Resort Name</th>
+            </tr>
+          </thead>
+          <tbody>
+                 <tr>
+                 <td>{getreceipt()['email']}</td>
+                  <td>{getreceipt()['id']}</td>
+                  <td>{getreceipt()['flight_num']}</td>
+                  <td>{getreceipt()['passenger_name']}</td>
+                  <td>{getreceipt()['fr_location']}</td>
+                  <td>{getreceipt()['to_location']}</td>
+                  <td>{getreceipt()['class']}</td>
+                  <td>{getreceipt()['flight_date']}</td>
+                  <td>{getreceipt()['flight_time']}</td>
+                  <td>{getreceipt()['gate_num']}</td>
+                  <td>{getreceipt()['board_time']}</td>
+                  <td>{getreceipt()['resort_name']}</td>
+                </tr>
+             
+      
+           
+            
+          </tbody>
+        </table>
+    }
     </div>
 );
 }
 }
 
-function Backhome(){
-  let history = useHistory();
-  return  history.push("/");
-}
 
 export default SignIn
