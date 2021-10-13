1a525444a9f9d95a6249df48a84e33a616f45c60
amaiYume <72297102+amaiYume@users.noreply.github.com>
2021-10-13 15:44:43 +0800

    Add files via upload

--- db/delete.php
+++ db/delete.php
@@ -1,24 +1,33 @@
 <?php
-header('Content-Type: application/json');
+/*To Get All Items in the Table (list.php).*/
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-if($_SERVER["REQUEST_METHOD"] == "OPTIONS") exit();
+header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS');
+
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
+
 include("db.php");
 
-$email = $_POST['email'];
-$stmt = $db->prepare("DELETE FROM receipt WHERE email = ?");
-$result = $stmt->execute([$email]);
+function setResponseCode($code) {
+    if($_SERVER["REQUEST_METHOD"] == "GET" || $_SERVER["REQUEST_METHOD"] == "POST") {
+        http_response_code($code);
+    }
+}
+
+$id = $_POST['id'];
+
+$stmt = $db->prepare("DELETE FROM users WHERE id = '$id'");
+$result = $stmt->execute();
+
 
 if($result){
-    echo json_encode([
-    'code' => '201'
-    ]);
+    setResponseCode(200);
+    echo json_encode($result);
 } else{
-    echo json_encode([
-        'code' => '400'
-        ]);
+    setResponseCode(404);
+    echo json_encode($result);
 };
+
 ?>
